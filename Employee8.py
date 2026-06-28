28dc3e65ff84899b847e3a5fb2f75a2604789e85
Manisha <manishaghuge2008@gmail.com>
2026-06-28 23:44:41 +0530

    updated code

--- Employee8.py
+++ Employee8.py
@@ -2,36 +2,34 @@ class Employee:
     def __init__(self, emp_id, name, details):
         self.emp_id = emp_id
         self.name = name
-        self.details = details   # tuple (department, salary)
+        self.details = details   
 
     def show_details(self):
-        print("\nEmployee ID:", self.emp_id)
+        
+        print("\n Employee ID:", self.emp_id)
         print("Name:", self.name)
         print("Department:", self.details[0])
         print("Salary:", self.details[1])
 
 
-# Dictionary to store employees
+
 employees = {}
 
 try:
-    # Adding 3 employees
+
     for i in range(3):
-        print(f"\nEnter details of Employee {i+1}")
+        print(f"\n Enter details of Employee {i+1}")
 
         emp_id = input("Enter Employee ID: ")
         name = input("Enter Name: ")
         dept = input("Enter Department: ")
         salary = float(input("Enter Salary: "))
 
-        # store department and salary in tuple
-        details = (dept, salary)
 
-        # create object and store in dictionary
         emp = Employee(emp_id, name, details)
         employees[emp_id] = emp
 
-    # Display all employees
+  
     print("\n----- Employee Details -----")
     for emp in employees.values():
         emp.show_details()
